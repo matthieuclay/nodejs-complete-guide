47ee3490befb85408cd36315f6b047ae2966e291
Matthieu Clay <matthieu.clay@outlook.fr>
2022-04-19 09:54:40 +0200

    :sparkles: using express router

--- app.js
+++ app.js
@@ -3,23 +3,12 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/add-product', (req, res, next) => {
-	console.log('In another middleware!');
-	res.send(
-		'<form action="/product" method="POST"><input type="text" name="title" /><button type="submit">Add product</button></form>',
-	);
-});
-
-app.post('/product', (req, res, next) => {
-	console.log(req.body);
-	res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-	console.log('In another middleware!');
-	res.send('<h1>Hello from Express!</h1>');
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3000);
