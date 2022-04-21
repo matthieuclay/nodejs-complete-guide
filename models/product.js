9d2882c1b986430bb3bb745a45f82ce7bbf3d247
Matthieu Clay <matthieu.clay@outlook.fr>
2022-04-21 13:52:58 +0200

    :zap: adding the product-delete functionality

--- models/product.js
+++ models/product.js
@@ -47,6 +47,18 @@ module.exports = class Product {
 		});
 	}
 
+	static deleteById(id) {
+		getProductsFromFile((products) => {
+			const updatedProducts = products.filter(
+				(product) => product.id !== id,
+			);
+			fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
+				if (!err) {
+				}
+			});
+		});
+	}
+
 	static fetchAll(cb) {
 		getProductsFromFile(cb);
 	}
