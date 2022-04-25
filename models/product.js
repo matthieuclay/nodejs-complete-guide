c7b229ef40b5524d7f47799b85bbc6d2120f9b2d
Matthieu Clay <matthieu.clay@outlook.fr>
2022-04-25 17:04:22 +0200

    :zap: fetching all products

--- models/product.js
+++ models/product.js
@@ -16,6 +16,19 @@ class Product {
 			.then((result) => console.log(result))
 			.catch((err) => console.error(err));
 	}
+
+	static fetchAll() {
+		const db = getDb();
+		return db
+			.collection('products')
+			.find()
+			.toArray()
+			.then((products) => {
+				console.log(products);
+				return products;
+			})
+			.catch((err) => console.error(err));
+	}
 }
 
 module.exports = Product;
