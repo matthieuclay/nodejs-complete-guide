582c309e2e65b20f3b2a83b9347354c7324d0e8f
Matthieu Clay <matthieu.clay@outlook.fr>
2022-04-25 11:24:18 +0200

    :sparkles: :zap: creating & managing a dummy user

--- app.js
+++ app.js
@@ -19,6 +19,15 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+	User.findByPk(1)
+		.then((user) => {
+			req.user = user;
+			next();
+		})
+		.catch((err) => console.error(err));
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
@@ -28,6 +37,20 @@ Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
 
 sequelize
-	.sync({ force: true })
-	.then((result) => app.listen(3000))
+	// .sync({ force: true })
+	.sync()
+	.then((result) => User.findByPk(1))
+	.then((user) => {
+		if (!user) {
+			return User.create({
+				name: 'Matthieu',
+				email: 'matthieu@test.com',
+			});
+		}
+		return user;
+	})
+	.then((user) => {
+		// console.log(user);
+		app.listen(3000);
+	})
 	.catch((err) => console.error(err));
