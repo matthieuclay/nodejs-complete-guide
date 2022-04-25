e58f4c6589c203149f74e9f23997ef9daf7335df
Matthieu Clay <matthieu.clay@outlook.fr>
2022-04-25 11:33:08 +0200

    :zap: fetching related products

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,7 +1,8 @@
 const Product = require('../models/product');
 
 exports.getProducts = (req, res, next) => {
-	Product.findAll()
+	req.user
+		.getProducts()
 		.then((products) => {
 			res.render('admin/products', {
 				products,
@@ -37,8 +38,10 @@ exports.getEditProduct = (req, res, next) => {
 		return res.redirect('/');
 	}
 	const productId = req.params.productId;
-	Product.findByPk(productId)
-		.then((product) => {
+	req.user
+		.getProducts({ where: { id: productId } })
+		.then((products) => {
+			const product = products[0];
 			if (!product) {
 				return res.redirect('/');
 			}
