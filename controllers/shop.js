e15b99c71fa6a873c43b2723f6c908faf9e911e5
Matthieu Clay <matthieu.clay@outlook.fr>
2022-04-25 13:56:24 +0200

    :zap: adding new products to the cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -68,10 +68,34 @@ exports.getCart = (req, res, next) => {
 
 exports.postCart = (req, res, next) => {
 	const productId = req.body.productId;
-	Product.findById(productId, (product) => {
-		Cart.addProduct(productId, product.price);
-	});
-	res.redirect('/cart');
+	let fetchedCart;
+	req.user
+		.getCart()
+		.then((cart) => {
+			fetchedCart = cart;
+			return cart.getProducts({ where: { id: productId } });
+		})
+		.then((products) => {
+			let product;
+			if (products.length > 0) {
+				product = products[0];
+			}
+			let newQuantity = 1;
+			if (product) {
+				// ...
+			}
+			return Product.findByPk(productId)
+				.then((product) => {
+					return fetchedCart.addProduct(product, {
+						through: { quantity: newQuantity },
+					});
+				})
+				.catch((err) => console.error(err));
+		})
+		.then(() => {
+			res.redirect('/cart');
+		})
+		.catch((err) => console.error(err));
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
