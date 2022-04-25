e99cf086c5f2853f2eafe4b1b1dabd0fd7e62a01
Matthieu Clay <matthieu.clay@outlook.fr>
2022-04-25 14:18:21 +0200

    :zap: adding existing products & retrieving cart items

--- controllers/shop.js
+++ controllers/shop.js
@@ -69,6 +69,7 @@ exports.getCart = (req, res, next) => {
 exports.postCart = (req, res, next) => {
 	const productId = req.body.productId;
 	let fetchedCart;
+	let newQuantity = 1;
 	req.user
 		.getCart()
 		.then((cart) => {
@@ -80,17 +81,17 @@ exports.postCart = (req, res, next) => {
 			if (products.length > 0) {
 				product = products[0];
 			}
-			let newQuantity = 1;
 			if (product) {
-				// ...
+				const oldQuantity = product.cartItem.quantity;
+				newQuantity = oldQuantity + 1;
+				return product;
 			}
-			return Product.findByPk(productId)
-				.then((product) => {
-					return fetchedCart.addProduct(product, {
-						through: { quantity: newQuantity },
-					});
-				})
-				.catch((err) => console.error(err));
+			return Product.findByPk(productId);
+		})
+		.then((product) => {
+			return fetchedCart.addProduct(product, {
+				through: { quantity: newQuantity },
+			});
 		})
 		.then(() => {
 			res.redirect('/cart');
