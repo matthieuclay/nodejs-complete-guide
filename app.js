2e8f3ad68ceaf5e1cbd40b98f8328e166d912ad0
Matthieu Clay <matthieu.clay@outlook.fr>
2022-04-26 11:02:19 +0200

    :zap: adding tje "add to cart" functionality

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
 	User.findById('6267af70517ff92bf3bef885')
 		.then((user) => {
-			req.user = user;
+			req.user = new User(user.username, user.email, user.cart, user._id);
 			next();
 		})
 		.catch((err) => console.error(err));
