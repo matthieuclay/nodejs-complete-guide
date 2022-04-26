6e97f741efec0f9f34fc1ec4ccbd666a1da47b35
Matthieu Clay <matthieu.clay@outlook.fr>
2022-04-26 09:41:12 +0200

    :zap: making the "edit" & "delete" buttons work again

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,8 +1,7 @@
 const Product = require('../models/product');
 
 exports.getProducts = (req, res, next) => {
-	req.user
-		.getProducts()
+	Product.fetchAll()
 		.then((products) => {
 			res.render('admin/products', {
 				products,
--- routes/admin.js
+++ routes/admin.js
@@ -13,7 +13,7 @@ router.get('/add-product', adminController.getAddProduct);
 // router.get('/edit-product/:productId', adminController.getEditProduct);
 
 // /admin/products => GET
-// router.get('/products', adminController.getProducts);
+router.get('/products', adminController.getProducts);
 
 // /admin/add-product => POST
 router.post('/add-product', adminController.postAddProduct);
