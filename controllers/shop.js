b24e2102eda552cbe727641f6f2d991eccfe5583
Matthieu Clay <matthieu.clay@outlook.fr>
2022-04-26 14:06:23 +0200

    :bug: fixing a bug

--- controllers/shop.js
+++ controllers/shop.js
@@ -58,36 +58,37 @@ exports.postCart = (req, res, next) => {
 		})
 		.then((result) => {
 			console.log(result);
-		})
-		// let fetchedCart;
-		// let newQuantity = 1;
-		// req.user
-		// 	.getCart()
-		// 	.then((cart) => {
-		// 		fetchedCart = cart;
-		// 		return cart.getProducts({ where: { id: productId } });
-		// 	})
-		// 	.then((products) => {
-		// 		let product;
-		// 		if (products.length > 0) {
-		// 			product = products[0];
-		// 		}
-		// 		if (product) {
-		// 			const oldQuantity = product.cartItem.quantity;
-		// 			newQuantity = oldQuantity + 1;
-		// 			return product;
-		// 		}
-		// 		return Product.findByPk(productId);
-		// 	})
-		// 	.then((product) => {
-		// 		return fetchedCart.addProduct(product, {
-		// 			through: { quantity: newQuantity },
-		// 		});
-		// 	})
-		// 	.then(() => {
-		// 		res.redirect('/cart');
-		// 	})
-		.catch((err) => console.error(err));
+			res.redirect('/cart');
+		});
+	// let fetchedCart;
+	// let newQuantity = 1;
+	// req.user
+	// 	.getCart()
+	// 	.then((cart) => {
+	// 		fetchedCart = cart;
+	// 		return cart.getProducts({ where: { id: productId } });
+	// 	})
+	// 	.then((products) => {
+	// 		let product;
+	// 		if (products.length > 0) {
+	// 			product = products[0];
+	// 		}
+	// 		if (product) {
+	// 			const oldQuantity = product.cartItem.quantity;
+	// 			newQuantity = oldQuantity + 1;
+	// 			return product;
+	// 		}
+	// 		return Product.findByPk(productId);
+	// 	})
+	// 	.then((product) => {
+	// 		return fetchedCart.addProduct(product, {
+	// 			through: { quantity: newQuantity },
+	// 		});
+	// 	})
+	// 	.then(() => {
+	// 		res.redirect('/cart');
+	// 	})
+	// .catch((err) => console.error(err));
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
