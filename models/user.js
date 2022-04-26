ae198f80c3e5717960a4515292f1cbf8d46cee74
Matthieu Clay <matthieu.clay@outlook.fr>
2022-04-26 10:53:11 +0200

    :sparkles: :zap: working on cart items & orders

--- models/user.js
+++ models/user.js
@@ -4,13 +4,30 @@ const getDb = require('../utils/database').getDb;
 const ObjectId = mongodb.ObjectId;
 
 class User {
-	constructor(username, email) {
-		(this.username = username), (this.email = email);
+	constructor(username, email, cart, _id) {
+		this.username = username;
+		this.email = email;
+		this.cart = cart; // {items: []}
+		this._id = _id;
 	}
 
 	save() {
 		const db = getDb();
 		return db.collection('users').insertOne(this);
+	}
+
+	addToCart(product) {
+		// const cartProduct = this.cart.items.findIndex(cp => {
+		// 	return cp._id === product._id
+		// });
+		const updatedCart = { items: [{ ...product, quantity: 1 }] };
+		const db = getDb();
+		return db
+			.collection('users')
+			.updateOne(
+				{ _id: new ObjectId(this._id) },
+				{ $set: { cart: updatedCart } },
+			);
 	}
 
 	static findById(userId) {
