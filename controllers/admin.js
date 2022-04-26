22ceaa437828ca6cddb7a341e00635e3f620b5c5
Matthieu Clay <matthieu.clay@outlook.fr>
2022-04-26 15:28:34 +0200

    :zap: deleting products

--- controllers/admin.js
+++ controllers/admin.js
@@ -74,7 +74,7 @@ exports.postEditProduct = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
 	const productId = req.body.productId;
-	Product.deleteById(productId)
+	Product.findByIdAndRemove(productId)
 		.then(() => res.redirect('/admin/products'))
 		.catch((err) => console.error(err));
 };
