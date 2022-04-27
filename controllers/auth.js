cf178cc43debfbf7e69f54ad758a5c97fc92698d
Matthieu Clay <matthieu.clay@outlook.fr>
2022-04-27 11:56:10 +0200

    :zap: using the session middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,15 @@
 exports.getLogin = (req, res, next) => {
-	const isLoggedIn =
-		req.get('Cookie').split(';')[0].trim().split('=')[1] === 'true';
+	// const isLoggedIn =
+	// 	req.get('Cookie').split(';')[0].trim().split('=')[1] === 'true';
+	console.log(req.session.isLoggedIn);
 	res.render('auth/login', {
 		pageTitle: 'Login',
 		path: '/login',
-		isAuthenticated: isLoggedIn,
+		isAuthenticated: false,
 	});
 };
 
 exports.postLogin = (req, res, next) => {
-	res.setHeader('Set-Cookie', 'loggedIn=true');
+	req.session.isLoggedIn = true;
 	res.redirect('/');
 };
