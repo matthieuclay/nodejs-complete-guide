8d87ca07e36986f69a237f078556507b56eb91fc
Matthieu Clay <matthieu.clay@outlook.fr>
2022-04-28 14:10:20 +0200

    :art: :zap: keeping user input

--- controllers/auth.js
+++ controllers/auth.js
@@ -40,6 +40,7 @@ exports.getSignup = (req, res, next) => {
 		pageTitle: 'Signup',
 		path: '/signup',
 		errorMessage: message,
+		oldInput: { email: '', password: '', confirmPassword: '' },
 	});
 };
 
@@ -87,7 +88,7 @@ exports.postLogin = (req, res, next) => {
 
 	const errors = validationResult(req);
 	if (!errors.isEmpty()) {
-		return res.render('auth/login', {
+		return res.status(422).render('auth/login', {
 			pageTitle: 'Login',
 			path: '/login',
 			errorMessage: errors.array()[0].msg,
@@ -124,6 +125,7 @@ exports.postLogin = (req, res, next) => {
 exports.postSignup = (req, res, next) => {
 	const email = req.body.email;
 	const password = req.body.password;
+	const confirmPassword = req.body.confirmPassword;
 
 	const errors = validationResult(req);
 	if (!errors.isEmpty()) {
@@ -131,6 +133,7 @@ exports.postSignup = (req, res, next) => {
 			pageTitle: 'Signup',
 			path: '/signup',
 			errorMessage: errors.array()[0].msg,
+			oldInput: { email, password, confirmPassword },
 		});
 	}
 
