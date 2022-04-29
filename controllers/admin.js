102375c054707ee7e0b5d0ce598599282af132ba
Matthieu Clay <matthieu.clay@outlook.fr>
2022-04-29 09:58:50 +0200

    :zap: handling file uploads with multer

--- controllers/admin.js
+++ controllers/admin.js
@@ -33,7 +33,7 @@ exports.getAddProduct = (req, res, next) => {
 
 exports.postAddProduct = (req, res, next) => {
 	const title = req.body.title;
-	const img = req.body.img;
+	const img = req.file;
 	const price = req.body.price;
 	const description = req.body.description;
 	const errors = validationResult(req);
