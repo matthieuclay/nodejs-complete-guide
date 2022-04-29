cd113429a0813f811d85efb26199da2d68c2f77c
Matthieu Clay <matthieu.clay@outlook.fr>
2022-04-29 15:20:02 +0200

    :heavy_plus_sign: :sparkles: using pdfkit for .pdf generation

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
+const PDFDocument = require('pdfkit');
+
 const Product = require('../models/product');
 const Order = require('../models/order');
 
@@ -154,6 +156,20 @@ exports.getInvoice = (req, res, next) => {
 			}
 			const invoiceName = 'invoice-' + orderId + '.pdf';
 			const invoicePath = path.join('data', 'invoices', invoiceName);
+
+			const pdfDoc = new PDFDocument();
+			res.setHeader('Content-Type', 'application/pdf');
+			res.setHeader(
+				'Content-Disposition',
+				'inline; filename="' + invoiceName + '"',
+			);
+			pdfDoc.pipe(fs.createWriteStream(invoicePath));
+			pdfDoc.pipe(res);
+
+			pdfDoc.text('Hello world!');
+
+			pdfDoc.end();
+
 			// fs.readFile(invoicePath, (err, data) => {
 			// 	if (err) {
 			// 		return next(err);
@@ -165,13 +181,6 @@ exports.getInvoice = (req, res, next) => {
 			// 	);
 			// 	res.send(data);
 			// });
-			const file = fs.createReadStream(invoicePath);
-			res.setHeader('Content-Type', 'application/pdf');
-			res.setHeader(
-				'Content-Disposition',
-				'inline; filename="' + invoiceName + '"',
-			);
-			file.pipe(res);
 		})
 		.catch((err) => next(err));
 };
