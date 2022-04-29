c8edcd6095ef601990d1a4fa5fb4ffcf34adb4e8
Matthieu Clay <matthieu.clay@outlook.fr>
2022-04-29 15:35:50 +0200

    :zap: generating .pdf files with order data

--- controllers/shop.js
+++ controllers/shop.js
@@ -166,7 +166,26 @@ exports.getInvoice = (req, res, next) => {
 			pdfDoc.pipe(fs.createWriteStream(invoicePath));
 			pdfDoc.pipe(res);
 
-			pdfDoc.text('Hello world!');
+			pdfDoc.fontSize(26).text('Invoice', {
+				underline: true,
+			});
+			pdfDoc.text('-----------------------');
+			let totalPrice = 0;
+			order.products.forEach((p) => {
+				totalPrice += p.quantity * p.product.price;
+				pdfDoc
+					.fontSize(14)
+					.text(
+						p.product.title +
+							' - ' +
+							p.quantity +
+							' x ' +
+							'$' +
+							p.product.price,
+					);
+			});
+			pdfDoc.text('---');
+			pdfDoc.fontSize(20).text('Total Price: $' + totalPrice);
 
 			pdfDoc.end();
 
