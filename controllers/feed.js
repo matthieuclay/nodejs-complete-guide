1662c30f37e217a2e9b59159db2f0c95768c7747
Matthieu Clay <matthieu.clay@outlook.fr>
2022-05-03 10:20:34 +0200

    :zap: storing posts in the database

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,5 +1,7 @@
 const { validationResult } = require('express-validator');
 
+const Post = require('../models/post');
+
 exports.getPosts = (req, res, next) => {
 	res.status(200).json({
 		posts: [
@@ -20,25 +22,25 @@ exports.getPosts = (req, res, next) => {
 exports.createPost = (req, res, next) => {
 	const errors = validationResult(req);
 	if (!errors.isEmpty()) {
-		return res
-			.status(422)
-			.json({
-				message: 'Validation failed, entered data is incorrect.',
-				errors: errors.array(),
-			});
+		return res.status(422).json({
+			message: 'Validation failed, entered data is incorrect.',
+			errors: errors.array(),
+		});
 	}
 	const title = req.body.title;
 	const content = req.body.content;
-
-	// Create post in DB
-	res.status(201).json({
-		message: 'Post created successfully',
-		post: {
-			_id: Date.now(),
-			title,
-			content,
-			creator: { name: 'Matthieu' },
-			createdAt: new Date(),
-		},
+	const post = new Post({
+		title,
+		content,
+		imageUrl: 'images/clay.png',
+		creator: { name: 'Matthieu' },
 	});
+	post.save()
+		.then((result) => {
+			res.status(201).json({
+				message: 'Post created successfully',
+				post: result,
+			});
+		})
+		.catch((err) => console.error(err));
 };
