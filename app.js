ba0f614778f849f16cb2b5e2adc33b31c26122f9
Matthieu Clay <matthieu.clay@outlook.fr>
2022-05-03 14:40:28 +0200

    :zap: adding a user model

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 const multer = require('multer');
 
 const feedRoutes = require('./routes/feed');
+const authRoutes = require('./routes/auth');
 
 const app = express();
 
@@ -49,6 +50,7 @@ app.use((req, res, next) => {
 });
 
 app.use('/feed', feedRoutes);
+app.use('/auth', authRoutes);
 
 app.use((error, req, res, next) => {
 	console.error(error);
