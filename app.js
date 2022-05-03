854fbf4d3fed13ad8b6a47658640072b09cb868e
Matthieu Clay <matthieu.clay@outlook.fr>
2022-05-03 14:55:26 +0200

    :zap: adding user signup validation

--- app.js
+++ app.js
@@ -56,7 +56,8 @@ app.use((error, req, res, next) => {
 	console.error(error);
 	const status = error.statusCode || 500;
 	const message = error.message;
-	res.status(status).json({ message });
+	const data = error.data;
+	res.status(status).json({ message, data });
 });
 
 mongoose
