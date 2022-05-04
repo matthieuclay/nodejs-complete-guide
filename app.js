f9e4f403b031712f5eb011bf33d219358aafee8d
Matthieu Clay <matthieu.clay@outlook.fr>
2022-05-04 11:14:16 +0200

    :zap: sharing the io instance across files

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ mongoose
 	.connect(process.env.MONGODB_CONNECT)
 	.then(() => {
 		const server = app.listen(8080);
-		const io = require('socket.io')(server, {
+		const io = require('./socket').init(server, {
 			cors: {
 				origin: 'http://localhost:3000',
 				methods: ['GET', 'POST'],
