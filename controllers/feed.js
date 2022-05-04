1afefd0db1e54aefa5675c2f33e512b45c884b97
Matthieu Clay <matthieu.clay@outlook.fr>
2022-05-04 09:26:27 +0200

    :zap: clearing post-user relations

--- controllers/feed.js
+++ controllers/feed.js
@@ -170,6 +170,13 @@ exports.deletePost = (req, res, next) => {
 			return Post.findByIdAndRemove(postId);
 		})
 		.then(() => {
+			return User.findById(req.userId);
+		})
+		.then((user) => {
+			user.posts.pull(postId);
+			return user.save();
+		})
+		.then(() => {
 			res.status(200).json({ message: 'Deleted post.' });
 		})
 		.catch((err) => {
