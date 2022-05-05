51134579ca66333ba4c954085b9584134854d082
Matthieu Clay <matthieu.clay@outlook.fr>
2022-05-05 15:12:19 +0200

    :white_check_mark: organizing multiple tests

--- test/auth-middleware.js
+++ test/auth-middleware.js
@@ -2,13 +2,24 @@ const expect = require('chai').expect;
 
 const authMiddleware = require('../middleware/is-auth');
 
-it('should throw an error if no authorization header is present', function () {
-	const req = {
-		get: function () {
-			return null;
-		},
-	};
-	expect(authMiddleware.bind(this, req, {}, () => {})).to.throw(
-		'Not authenticated.',
-	);
+describe('auth-middleware.js', function () {
+	it('should throw an error if no authorization header is present', function () {
+		const req = {
+			get: function () {
+				return null;
+			},
+		};
+		expect(authMiddleware.bind(this, req, {}, () => {})).to.throw(
+			'Not authenticated.',
+		);
+	});
+
+	it('should throw an error if the authorization header is only one string', function () {
+		const req = {
+			get: function () {
+				return 'azerty';
+			},
+		};
+		expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+	});
 });
