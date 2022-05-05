aeb647830030fded4b187bb4ec19c82fe67cf6ed
Matthieu Clay <matthieu.clay@outlook.fr>
2022-05-05 14:55:30 +0200

    :sparkles: :zap: setup and writing a first test

--- routes/auth.js
+++ routes/auth.js
@@ -1,77 +1,41 @@
 const express = require('express');
-const { check, body } = require('express-validator');
+const { body } = require('express-validator');
 
-const authController = require('../controllers/auth');
 const User = require('../models/user');
+const authController = require('../controllers/auth');
+const isAuth = require('../middleware/is-auth');
 
 const router = express.Router();
 
-router.get('/login', authController.getLogin);
-
-router.get('/signup', authController.getSignup);
-
-router.get('/reset', authController.getReset);
-
-router.get('/reset/:token', authController.getNewPassword);
-
-router.post(
-	'/login',
+router.put(
+	'/signup',
 	[
 		body('email')
 			.isEmail()
-			.withMessage('Please enter a valid email address.')
-			.normalizeEmail(),
-		body('password', 'Password has to be valid.')
-			.isLength({ min: 5 })
-			.isAlphanumeric()
-			.trim(),
-	],
-	authController.postLogin,
-);
-
-router.post(
-	'/signup',
-	[
-		check('email')
-			.isEmail()
-			.withMessage('Please enter a valid email address.')
+			.withMessage('Please enter a valid email.')
 			.custom((value, { req }) => {
-				// if (value === 'test@test.com') {
-				// 	throw new Error('This email address is forbidden.');
-				// }
-				// return true;
 				return User.findOne({ email: value }).then((userDoc) => {
 					if (userDoc) {
-						return Promise.reject(
-							'Email exists already, please pick a different one.',
-						);
+						return Promise.reject('E-Mail address already exists!');
 					}
 				});
 			})
 			.normalizeEmail(),
-		body(
-			'password',
-			'Please enter a password with only numbers and text and at least 5 characters.',
-		)
-			.isLength({ min: 5 })
-			.isAlphanumeric()
-			.trim(),
-		body('confirmPassword')
-			.trim()
-			.custom((value, { req }) => {
-				if (value !== req.body.password) {
-					throw new Error('Passwords have to match!');
-				}
-				return true;
-			}),
+		body('password').trim().isLength({ min: 5 }),
+		body('name').trim().not().isEmpty(),
 	],
-	authController.postSignup,
+	authController.signup,
 );
 
-router.post('/reset', authController.postReset);
+router.post('/login', authController.login);
 
-router.post('/new-password', authController.postNewPassword);
+router.get('/status', isAuth, authController.getUserStatus);
 
-router.post('/logout', authController.postLogout);
+router.patch(
+	'/status',
+	isAuth,
+	[body('status').trim().not().isEmpty()],
+	authController.updateUserStatus,
+);
 
 module.exports = router;
