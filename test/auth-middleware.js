013bedaba6bded491ae6243dfe50bc57cde899f7
Matthieu Clay <matthieu.clay@outlook.fr>
2022-05-05 15:20:55 +0200

    :white_check_mark: what not to test

--- test/auth-middleware.js
+++ test/auth-middleware.js
@@ -22,4 +22,23 @@ describe('auth-middleware.js', function () {
 		};
 		expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
 	});
+
+	it('should throw an error if the token cannot be verified', function () {
+		const req = {
+			get: function () {
+				return 'Bearer abc';
+			},
+		};
+		expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+	});
+
+	it('should yield a userId after decoding the token', function () {
+		const req = {
+			get: function () {
+				return 'Bearer abc';
+			},
+		};
+		authMiddleware(req, {}, () => {});
+		expect(req).to.have.property('userId');
+	});
 });
