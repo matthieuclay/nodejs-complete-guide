a44502163a1666c6af446f6f6a6e70dafca7a827
Matthieu Clay <matthieu.clay@outlook.fr>
2022-05-05 15:37:38 +0200

    :white_check_mark: using stubs

--- test/auth-middleware.js
+++ test/auth-middleware.js
@@ -1,4 +1,6 @@
 const expect = require('chai').expect;
+const jwt = require('jsonwebtoken');
+const sinon = require('sinon');
 
 const authMiddleware = require('../middleware/is-auth');
 
@@ -23,6 +25,21 @@ describe('auth-middleware.js', function () {
 		expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
 	});
 
+	it('should yield a userId after decoding the token', function () {
+		const req = {
+			get: function () {
+				return 'Bearer abc';
+			},
+		};
+		sinon.stub(jwt, 'verify');
+		jwt.verify.returns({ userId: 'abc' });
+		authMiddleware(req, {}, () => {});
+		expect(req).to.have.property('userId');
+		expect(req).to.have.property('userId', 'abc');
+		expect(jwt.verify.called).to.be.true;
+		jwt.verify.restore();
+	});
+
 	it('should throw an error if the token cannot be verified', function () {
 		const req = {
 			get: function () {
@@ -30,15 +47,5 @@ describe('auth-middleware.js', function () {
 			},
 		};
 		expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
-	});
-
-	it('should yield a userId after decoding the token', function () {
-		const req = {
-			get: function () {
-				return 'Bearer abc';
-			},
-		};
-		authMiddleware(req, {}, () => {});
-		expect(req).to.have.property('userId');
 	});
 });
