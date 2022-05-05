7a79b8f553bcecfd457604f8b69a504ee6cffca1
Matthieu Clay <matthieu.clay@outlook.fr>
2022-05-05 16:13:49 +0200

    :white_check_mark: setting up a testing database

--- test/auth-controller.js
+++ test/auth-controller.js
@@ -1,5 +1,6 @@
 const expect = require('chai').expect;
 const sinon = require('sinon');
+const mongoose = require('mongoose');
 
 const User = require('../models/user');
 const authController = require('../controllers/auth');
@@ -24,5 +25,21 @@ describe('auth-controller.js - login', function () {
 			});
 
 		User.findOne.restore();
+	});
+
+	it('should send a response with a valid user status for an existing user', function (done) {
+		mongoose
+			.connect(process.env.MONGODB_TEST_CONNECT)
+			.then(() => {
+				const user = new User({
+					email: 'test@test.com',
+					password: 'test123',
+					name: 'test',
+					posts: [],
+				});
+				return user.save();
+			})
+			.then(() => {})
+			.catch((err) => console.error(err));
 	});
 });
