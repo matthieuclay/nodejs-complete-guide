c40615baf5ad6fec9edeee30cfdb66f415634cb2
Matthieu Clay <matthieu.clay@outlook.fr>
2022-05-06 09:49:55 +0200

    :sparkles: typescript setup

--- app.js
+++ app.js
@@ -1,11 +1,2 @@
-// const express = require('express');
-import express from 'express';
-
-// const resHandler = require('./response-handler');
-import { resHandler } from './response-handler.js';
-
-const app = express();
-
-app.get('/', resHandler);
-
-app.listen(3000);
+// For convert .ts to .js, run this command:
+// tsc app.js
