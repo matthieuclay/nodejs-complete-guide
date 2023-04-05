e4f024c00ee37dae3e4671cf29feae878671d885
Matthieu Clay <matthieu.clay@outlook.fr>
2023-04-05 11:26:05 +0200

    using the runtime api

--- app.ts
+++ app.ts
@@ -1,3 +1,8 @@
-let message: string;
-message = 'Hi there!';
-console.log(message);
+const text = 'This is a test - and it should be stored in a file!';
+
+const encoder = new TextEncoder();
+const data = encoder.encode(text);
+
+Deno.writeFile('message.txt', data).then(() => {
+	console.log('Wrote to file!');
+});
